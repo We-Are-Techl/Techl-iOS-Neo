5fd852ab939a1a8fc18ff51dd29626d79760805b
Doy Kim <kimdee.dev@gmail.com>
2022-10-09 23:23:26 +0900

    Initial Commit

---
+++ Techl-Neo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techl-Neo
+//
+//  Created by Doy Kim on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
