1227d966c7857543e73eba96d2bd1067fd87b547
J.H. Moon <moonjh587@naver.com>
2023-01-22 01:59:14 +0900

    [Setting] - #22  베이스 뷰, 베이스VC의 기본배경색 처리 위치 변경

--- Techl-Neo/Presentations/Base/BaseVC.swift
+++ Techl-Neo/Presentations/Base/BaseVC.swift
@@ -14,7 +14,6 @@ class BaseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemBackground
         configure()
         setNavigationBar()
 
