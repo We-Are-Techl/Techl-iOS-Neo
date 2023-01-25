0bce27e9b01e62bf908068f7e569ab6ef60d37ce
J.H. Moon <87518434+ILWAT@users.noreply.github.com>
2023-01-25 23:49:10 +0900

    Revert "Design/#17 - 온보딩뷰, VC 파일 구성 및 화면 구현 / BaseView & BaseVC BackgroundColor 설정 위치 변경"

--- Techl-Neo/Presentations/Base/BaseVC.swift
+++ Techl-Neo/Presentations/Base/BaseVC.swift
@@ -14,6 +14,7 @@ class BaseVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = .systemBackground
         configure()
         setNavigationBar()
 
