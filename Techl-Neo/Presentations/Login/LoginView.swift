b4a43314a3843260fae24e3fe7b49b756ab793b1
J.H. Moon <moonjh587@naver.com>
2023-03-26 23:59:25 +0900

    [Design] - #19 login view

--- Techl-Neo/Presentations/Login/LoginView.swift
+++ Techl-Neo/Presentations/Login/LoginView.swift
@@ -24,6 +24,16 @@ class LoginView: BaseView {
         $0.textContentType = .telephoneNumber
     }
     
+    let phoneNumberGuideLabel = UILabel().then {
+        $0.text = "❌ 휴대전화번호는 '-' 없이 11자리로 입력해주세요"
+        $0.font = .systemFont(ofSize: 12)
+    }
+    
+    let passwordGuideLabel = UILabel().then {
+        $0.text = "❌ 비밀번호는 알파벳 + 숫자 포함 8자리 이상입니다"
+        $0.font = .systemFont(ofSize: 12)
+    }
+    
     
     //MARK: - setupUI
     override func setupUI() {
